ab91b9ba667b7408ead424946884bc5b2fc7d956
Pavan <pavanvenkatesh94@gmail.com>
2017-04-27 13:00:27 +0530

    :sparkles: Removed .idea folder

---
+++ carouselpicker/src/main/java/in/goodiebag/carouselpicker/CustomPageTransformer.java
@@ -0,0 +1,29 @@
+package in.goodiebag.carouselview;
+
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by pavan on 25/04/17.
+ */
+
+import android.content.Context;
+
+public class CustomPageTransformer implements ViewPager.PageTransformer {
+
+    private ViewPager viewPager;
+
+    public CustomPageTransformer(Context context) {
+    }
+
+    public void transformPage(View view, float position) {
+        if (viewPager == null) {
+            viewPager = (ViewPager) view.getParent();
+        }
+        view.setScaleY(1-Math.abs(position));
+        view.setScaleX(1-Math.abs(position));
+        Log.d("position", "" + view.getTag()+ "Co-ordinate position is " +  position + "\n");
+    }
+
+}
