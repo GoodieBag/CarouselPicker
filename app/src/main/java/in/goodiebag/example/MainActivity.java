5bb82d41fe1a021447fcc982cf3df61b69570b57
Pavan <pavanvenkatesh94@gmail.com>
2017-05-02 13:20:48 +0530

    :sparkles: Added support for text and images.

--- app/src/main/java/in/goodiebag/example/MainActivity.java
+++ app/src/main/java/in/goodiebag/example/MainActivity.java
@@ -1,7 +1,9 @@
 package in.goodiebag.example;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,28 +12,92 @@ import in.goodiebag.carouselpicker.CarouselPicker;
 import in.goodiebag.example.carouselview.R;
 
 public class MainActivity extends AppCompatActivity {
-    CarouselPicker carouselPicker;
+    CarouselPicker imageCarousel, textCarousel, mixCarousel;
+    TextView tvSelected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        carouselPicker = (CarouselPicker) findViewById(R.id.view);
-        List<Integer> items = new ArrayList<>();
-        items.add(R.drawable.icon_checkbox_tune);
-        items.add(R.drawable.icon_checkbox_tune);
-        items.add(R.drawable.icon_checkbox_tune);
-        items.add(R.drawable.icon_checkbox_tune);
-        items.add(R.drawable.icon_checkbox_tune);
-        items.add(R.drawable.icon_checkbox_tune);
-        items.add(R.drawable.icon_checkbox_tune);
-        items.add(R.drawable.icon_checkbox_tune);
-        items.add(R.drawable.icon_checkbox_tune);
-        items.add(R.drawable.icon_checkbox_tune);
-        items.add(R.drawable.icon_checkbox_tune);
-        items.add(R.drawable.icon_checkbox_tune);
-        CarouselPicker.CarouselViewAdapter adapter = new CarouselPicker.CarouselViewAdapter(this, items, 0);
-        carouselPicker.setAdapter(adapter);
+        imageCarousel = (CarouselPicker) findViewById(R.id.imageCarousel);
+        textCarousel = (CarouselPicker) findViewById(R.id.textCarousel);
+        mixCarousel = (CarouselPicker) findViewById(R.id.mixCarousel);
+        tvSelected = (TextView) findViewById(R.id.tvSelectedItem);
 
+        List<CarouselPicker.PickerItem> imageItems = new ArrayList<>();
+        imageItems.add(new CarouselPicker.DrawableItem(R.drawable.icon_checkbox_tune));
+        imageItems.add(new CarouselPicker.DrawableItem(R.drawable.icon_checkbox_tune));
+        imageItems.add(new CarouselPicker.DrawableItem(R.drawable.icon_checkbox_tune));
+        imageItems.add(new CarouselPicker.DrawableItem(R.drawable.icon_checkbox_tune));
+        CarouselPicker.CarouselViewAdapter imageAdapter = new CarouselPicker.CarouselViewAdapter(this, imageItems, 0);
+        imageCarousel.setAdapter(imageAdapter);
+
+        List<CarouselPicker.PickerItem> textItems = new ArrayList<>();
+        textItems.add(new CarouselPicker.TextItem("hi", 20));
+        textItems.add(new CarouselPicker.TextItem("hi", 20));
+        textItems.add(new CarouselPicker.TextItem("hi", 20));
+        textItems.add(new CarouselPicker.TextItem("hi", 20));
+        CarouselPicker.CarouselViewAdapter textAdapter = new CarouselPicker.CarouselViewAdapter(this, textItems, 0);
+        textCarousel.setAdapter(textAdapter);
+
+        List<CarouselPicker.PickerItem> mixItems = new ArrayList<>();
+        mixItems.add(new CarouselPicker.DrawableItem(R.drawable.icon_checkbox_tune));
+        mixItems.add(new CarouselPicker.TextItem("hi", 20));
+        mixItems.add(new CarouselPicker.DrawableItem(R.drawable.icon_checkbox_tune));
+        mixItems.add(new CarouselPicker.TextItem("hi", 20));
+
+        CarouselPicker.CarouselViewAdapter mixAdapter = new CarouselPicker.CarouselViewAdapter(this, mixItems, 0);
+        mixCarousel.setAdapter(mixAdapter);
+
+        imageCarousel.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                tvSelected.setText("Selected item in image carousel is  : "+position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        textCarousel.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                tvSelected.setText("Selected item in text carousel is  : "+position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        mixCarousel.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                tvSelected.setText("Selected item in mix carousel is  : "+position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 }
--- carouselpicker/src/main/java/in/goodiebag/carouselpicker/CarouselPicker.java
+++ carouselpicker/src/main/java/in/goodiebag/carouselpicker/CarouselPicker.java
@@ -2,16 +2,17 @@ package in.goodiebag.carouselpicker;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.drawable.Drawable;
 import android.support.annotation.DrawableRes;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,9 +22,7 @@ import java.util.List;
  */
 
 public class CarouselPicker extends ViewPager {
-    private final float DENSITY = getContext().getResources().getDisplayMetrics().density;
     private int itemsVisible = 3;
-    private int itemWidth = ViewGroup.LayoutParams.WRAP_CONTENT;
     private float divisor;
 
     public CarouselPicker(Context context) {
@@ -37,7 +36,6 @@ public class CarouselPicker extends ViewPager {
     }
 
     private void initAttributes(Context context, AttributeSet attrs) {
-
         if (attrs != null) {
             final TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.CarouselPicker);
             itemsVisible = array.getInteger(R.styleable.CarouselPicker_items_visible, itemsVisible);
@@ -73,6 +71,16 @@ public class CarouselPicker extends ViewPager {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int height = 0;
+        for(int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+            child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+            int h = child.getMeasuredHeight();
+            if(h > height) height = h;
+        }
+
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         int w = getMeasuredWidth();
         setPageMargin((int) (-w / divisor));
@@ -87,18 +95,19 @@ public class CarouselPicker extends ViewPager {
 
     public static class CarouselViewAdapter extends PagerAdapter {
 
-        List<Integer> items = new ArrayList<>();
+        List<PickerItem> items = new ArrayList<>();
         Context context;
         int drawable;
 
-        public CarouselViewAdapter(Context context, List<Integer> items, @DrawableRes int drawable) {
+        public CarouselViewAdapter(Context context, List<PickerItem> items, int drawable) {
             this.context = context;
-            this.items = items;
             this.drawable = drawable;
+            this.items = items;
             if (this.drawable == 0) {
                 this.drawable = R.layout.page;
             }
         }
+
 
         @Override
         public int getCount() {
@@ -109,7 +118,23 @@ public class CarouselPicker extends ViewPager {
         public Object instantiateItem(ViewGroup container, int position) {
             View view = LayoutInflater.from(context).inflate(this.drawable, null);
             ImageView iv = (ImageView) view.findViewById(R.id.iv);
-            iv.setImageResource(items.get(position));
+            TextView tv = (TextView) view.findViewById(R.id.tv);
+            PickerItem pickerItem = items.get(position);
+            if (pickerItem.hasDrawable()) {
+                iv.setVisibility(VISIBLE);
+                tv.setVisibility(GONE);
+                iv.setImageResource(pickerItem.getDrawable());
+            } else {
+                if (pickerItem.getText() != null) {
+                    iv.setVisibility(GONE);
+                    tv.setVisibility(VISIBLE);
+                    tv.setText(pickerItem.getText());
+                    int textSize = ((TextItem)pickerItem).getTextSize();
+                    if(textSize != 0) {
+                        tv.setTextSize(dpToPx(((TextItem) pickerItem).getTextSize()));
+                    }
+                }
+            }
             view.setTag(position);
             container.addView(view);
             return view;
@@ -125,6 +150,86 @@ public class CarouselPicker extends ViewPager {
             return (view == object);
         }
 
+        private  int dpToPx(int dp) {
+            DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+            return Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+        }
+
     }
+
+    /**
+     * An interface which should be implemented by all the Item classes.
+     * The picker only accepts items in the form of PickerItem.
+     */
+    public interface PickerItem {
+        String getText();
+
+        @DrawableRes
+        int getDrawable();
+
+        boolean hasDrawable();
+    }
+
+    /**
+     * A PickerItem which supports text.
+     */
+    public static class TextItem implements PickerItem {
+        private String text;
+        private int textSize;
+        public TextItem(String text, int textSize) {
+            this.text = text;
+            this.textSize = textSize;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        @Override
+        public int getDrawable() {
+            return 0;
+        }
+
+        @Override
+        public boolean hasDrawable() {
+            return false;
+        }
+
+        public int getTextSize() {
+            return textSize;
+        }
+
+        public void setTextSize(int textSize) {
+            this.textSize = textSize;
+        }
+    }
+
+    /**
+     * A PickerItem which supports drawables.
+     */
+    public static class DrawableItem implements PickerItem {
+        @DrawableRes
+        private int drawable;
+
+        public DrawableItem(@DrawableRes int drawable) {
+            this.drawable = drawable;
+        }
+
+        @Override
+        public String getText() {
+            return null;
+        }
+
+        @DrawableRes
+        public int getDrawable() {
+            return drawable;
+        }
+
+        @Override
+        public boolean hasDrawable() {
+            return true;
+        }
+    }
+
 
 }
